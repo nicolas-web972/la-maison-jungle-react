b5f0d1e57d433ef61bcf043d9046634c27d527db
nicolas-web972 <nicolas.chalono@gmail.com>
2022-03-23 17:37:03 +0100

    components created and events

---
+++ src/datas/plantList.js
@@ -0,0 +1,56 @@
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+        isBestSale: true,
+        light: 2
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+        isSpecialOffer: true,
+        light: 2
+	},
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+        light: 2
+	},
+	{
+		name: 'yucca',
+		category: 'classique',
+		id: '4kk',
+        light: 2,
+        water: 1
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+        isSpecialOffer: true
+	},
+	{
+		name: 'géranium',
+		category: 'extérieur',
+		id: '6uo'
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+        isBestSale: true
+	},
+	{
+		name: 'aloe',
+		category: 'plante grasse',
+		id: '8fp'
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn'
+	}
+]
